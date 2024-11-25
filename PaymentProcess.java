fee685fb9552ac155959bdb0c8eed041d7b2260e
Usman Mahmood <usmanmahmood254@gmail.com>
2024-11-25 11:54:05 -0700

    Added some Data Componnts

---
+++ PaymentProcess.java
@@ -0,0 +1,59 @@
+
+import java.time.*;
+
+class Payment {
+    
+    private int PaymentID;
+    private static int payIdValue = 1;
+    private double Amount;
+    private DateTime PaymentDate;
+
+    public Payment(double Amount) {
+        this.PaymentID = payIdValue++;
+        this.Amount = Amount;
+        this.PaymentDate = new DateTime(); // The object DateTime is "ctored" here
+    }
+
+    public int getPaymentID() {
+        return PaymentID;
+    }
+    public double getAmount() {
+        return Amount;
+    }
+    public DateTime getPaymentDate() {
+        return PaymentDate;
+    }
+
+    public void setAmount(double Amount) {
+        this.Amount = Amount;
+    }
+}
+
+class Recepit {
+
+    private String ReceiptID;
+    private Ticket TicketID;
+}
+
+class DateTime {
+    private String day;
+    private String month;
+    private String year;
+
+    public DateTime() {
+        LocalDate currentDate = LocalDate.now();
+        this.day = String.valueOf(currentDate.getDayOfMonth());
+        this.month = String.valueOf(currentDate.getMonthValue());
+        this.year = String.valueOf(currentDate.getYear());
+    }
+
+    public String getDay() {
+        return day;
+    }
+    public String getMonth() {
+        return month;
+    }
+    public String getYear() {
+        return year;
+    }
+}
--- UserTypes.java
+++ UserTypes.java
@@ -1,9 +1,42 @@
-class Standard_User {
+
+class User {
+
+    private String name;
+    private int UserID;
+    private String email;
+    private Payment PaymentInfo;
+    private boolean isRegistered;
+
+    public User(String name, int UserID, String email) {
+        this.name = name;
+        this.UserID = UserID;
+        this.email = email;
+    }
+
+    public User() {}
+
+    public void destroy() {}
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+}
+
+class Registered_User extends User {
+
+    private float MembershipFee;
+    private String RegistrationDate;
+    private String ExpirationDate;
+    private boolean isAdminFeeExempt;
+    private int ReservedSeatCount;
+    private boolean newsBeforePublic;
+}
+
+class Manager {
 
 }
 
-class Registered_User {
 
-}
-
-
+// Theatre, movie, seats
+// payments, recepits, ticket (datetime)
