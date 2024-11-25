9477aaa362532b9eeaef8d191137bbd3d9c6ca32
Usman Mahmood <usmanmahmood254@gmail.com>
2024-11-25 14:56:31 -0700

    Made some updates

--- UserTypes.java
+++ UserTypes.java
@@ -1,17 +1,22 @@
 
 class User {
 
-    private String name;
-    private int UserID;
-    private static int IdValue = 20;
-    private String email;
-    private Payment PaymentInfo;
-    private boolean isRegistered;
+    protected String name;
+    protected int UserID;
+    protected static int IdValue = 20;
+    protected String email;
+    protected Payment PaymentInfo;
+    protected boolean isRegistered;
+    protected boolean newsBeforePublic;
+    protected boolean isAdminFeeExempt;
 
-    public User(String name, int UserID, String email) {
+    public User(String name, String email) {
         this.name = name;
         this.UserID = IdValue++;
         this.email = email;
+        this.isRegistered = false;
+        this.newsBeforePublic = false;
+        this.isAdminFeeExempt = false;
     }
     public User() {}
 
@@ -25,17 +30,47 @@ class User {
     public int getUserID() {
         return UserID;
     }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getEmail() {
+        return email;
+    }
+
+    public void setPaymentInfo(Payment PaymentInfo) {
+        this.PaymentInfo = PaymentInfo;
+    }
+    public Payment getPaymentInfo() {
+        return PaymentInfo;
+    }
     
 }
 
 class Registered_User extends User {
 
-    private float MembershipFee;
+    private static double MembershipFee = 20;
     private String RegistrationDate;
+    private String address;
     private String ExpirationDate;
-    private boolean isAdminFeeExempt;
     private int ReservedSeatCount;
-    private boolean newsBeforePublic;
+    private Card creditCardInfo;
+
+    public Registered_User(String name, String email, String address, Card creditCardInfo) {
+        this.name = name;
+        this.email = email;
+        this.address = address;
+        this.creditCardInfo = creditCardInfo;
+        this.UserID = IdValue++;
+        this.isRegistered = true;
+        this.newsBeforePublic = true;
+        this.isAdminFeeExempt = true;
+    }
+
+}
+
+class Card {
+
 }
 
 class Manager {
@@ -45,3 +80,5 @@ class Manager {
 
 // Theatre, movie, seats
 // payments, recepits, ticket (datetime)
+
+// entity: user, movie, therate
